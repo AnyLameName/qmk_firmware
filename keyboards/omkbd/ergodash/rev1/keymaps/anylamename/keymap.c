944a706de2370d7f3e394850e1aeead50aa3d972
Jay Gagnon <j.gagnon@jobtarget.com>
2024-02-29 10:09:53 -0500

    Adding HACK button, comments formatting. Also a dictionary for Rider to recognize some abbreviations.

--- keyboards/omkbd/ergodash/rev1/keymaps/anylamename/keymap.c
+++ keyboards/omkbd/ergodash/rev1/keymaps/anylamename/keymap.c
@@ -6,7 +6,8 @@ enum layer_names {
     _RAISE,
     _LOWER,
     _ADJUST,
-    _CODE
+    _CODE,
+    _HACK,
 };
 
 enum custom_keycodes {
@@ -15,13 +16,14 @@ enum custom_keycodes {
   LOWER,
   ADJUST,
   CODE,
+  HACK,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   /* Qwerty
    * ,----------------------------------------------------------------------------------------------------------------------.
-   * | Esc  |   1  |   2  |   3  |   4  |   5  |      |                    |      |   6  |   7  |   8  |   9  |   0  |Pscree|
+   * | Esc  |   1  |   2  |   3  |   4  |   5  |      |                    |      |   6  |   7  |   8  |   9  |   0  |Pscrn |
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
    * |  `   |   Q  |   W  |   E  |   R  |   T  |      |                    |      |   Y  |   U  |   I  |   O  |   P  |  \   |
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
@@ -37,37 +39,37 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GRV,  KC_Q,    KC_W,    KC_E, KC_R, KC_T,  KC_NO,                         KC_NO,  KC_Y,  KC_U, KC_I,    KC_O,    KC_P,    KC_BSLS,
     KC_ESC,  KC_A,    KC_S,    KC_D, KC_F, KC_G,  KC_NO,                         KC_NO,  KC_H,  KC_J, KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_C, KC_V, KC_B,  KC_NO,                         KC_NO,  KC_N,  KC_M, KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-    KC_LCTL, KC_LGUI, KC_LALT, CODE,       LOWER, KC_BSPC, KC_LSFT,     KC_RSFT, KC_SPC, RAISE,       KC_LEFT, KC_DOWN, KC_UP,   KC_LCTL
+    KC_LCTL, KC_LGUI, KC_LALT, CODE,       LOWER, KC_BSPC, KC_LSFT,     KC_RSFT, KC_SPC, RAISE,       HACK,    KC_DOWN, KC_UP,   KC_LCTL
   ),
 
   /* Raise
    * ,----------------------------------------------------------------------------------------------------------------------.
    * |  F11 |  F1  |  F2  |  F3  |  F4  |  F5  |      |                    |      |  F6  |  F7  |  F8  |  F9  |  F10 |  F12 |
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
-   * |  ~   |   !  |   @  |   #  |   $  |   _  |      |                    |      |   +  | Home | End  |   (  |   )  |  |   |
+   * |  ~   |   !  |   @  |   {  |   }  |   -  |      |                    |      |   =  | Home | End  |   (  |   )  |  |   |
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
-   * | Tab  |   1  |   2  |   3  |   4  |   5  |      |                    |      | Left | Down |  Up  | Right|   :  |  "   |
+   * | Tab  |   1  |   2  |   (  |   )  |   5  |      |                    |      | Left | Enter|  Up  | Right|   :  |  "   |
    * |------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------+------|
-   * | Shift|   6  |   7  |   8  |   9  |   {  |      |             |      |      |   }  |PageUp|PageDn|  End |   ?  | Shift|
+   * | Shift|   6  |   7  |   [  |   ]  |   {  |      |             |      |      |   }  |PageUp|PageDn|  End |   ?  | Shift|
    * |-------------+------+------+------+------+  Del +------+------+------+ Space+------+------+------+------+-------------|
    * | Ctrl |  GUI |  Alt | Code |||||||| Lower|      | Shift|||||||| Shift|      | Raise|||||||| Home |PageDn|PageUp|  End |
    * ,----------------------------------------------------------------------------------------------------------------------.
    */
   [_RAISE] = LAYOUT(
-    KC_F11,  KC_F1,   KC_F2,   KC_F3,   KC_F4,  KC_F5,   _______,                        _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F12 ,
-    KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR, KC_UNDS, _______,                        _______, KC_PLUS, KC_HOME, KC_END,  KC_LPRN, KC_RPRN, KC_PIPE,
-    KC_TAB,  KC_1,    KC_2,    KC_3,    KC_4,   KC_5,    _______,                        _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_COLN, KC_DQT ,
-    KC_LSFT, KC_6,    KC_7,    KC_8,    KC_9,   KC_LCBR, _______,                        _______, KC_RCBR, KC_PGUP, KC_PGDN, KC_END,  KC_QUES, KC_RSFT,
-    KC_LCTL, KC_LGUI, KC_LALT, CODE,            LOWER,   KC_DEL,  KC_LSFT,      KC_RSFT,  KC_SPC, RAISE,            KC_HOME, KC_PGDN, KC_PGUP, KC_END
+    KC_F11,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______,                        _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F12 ,
+    KC_TILD, KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_MINS, _______,                        _______, KC_EQL,  KC_HOME, KC_END,  KC_LPRN, KC_RPRN, KC_PIPE,
+    KC_TAB,  KC_1,    KC_2,    KC_LPRN, KC_RPRN, KC_5,    _______,                        _______, KC_LEFT, KC_ENT,  KC_UP,   KC_RGHT, KC_COLN, KC_DQT ,
+    KC_LSFT, KC_6,    KC_7,    KC_LBRC, KC_RBRC, KC_LCBR, _______,                        _______, KC_RCBR, KC_PGUP, KC_PGDN, KC_END,  KC_QUES, KC_RSFT,
+    KC_LCTL, KC_LGUI, KC_LALT, CODE,             LOWER,   KC_DEL,  KC_LSFT,      KC_RSFT,  KC_SPC, RAISE,            HACK,    KC_PGDN, KC_PGUP, KC_END
   ),
 
   /* Lower
    * ,-----------------------------------------------------------------------------------------------------------------------.
    * |  F11 |  F1  |  F2  |  F3  |  F4  |  F5  |       |                    |      |  F6  |  F7  |  F8  |  F9  |  F10 |  F12 |
    * |------+------+------+------+------+------+-------+--------------------+------+------+------+------+------+------+------|
-   * |  ~   |   !  |   @  |   #  |   $  |   -  |       |                    |      |   =  |   &  |   *  |   (  |   )  |  |   |
+   * |  ~   |   !  |   @  |   #  |   $  |   _  |       |                    |      |   +  |   &  |   *  |   (  |   )  |  |   |
    * |------+------+------+------+------+------+-------+--------------------+------+------+------+------+------+------+------|
-   * | Tab  |   1  |   2  |   3  |  Tab |   [  |       |                    |      |   ]  | Enter|   K  |   L  |   :  |  "   |
+   * | Tab  |   1  |   2  |   3  |  Tab |   [  |       |                    |      | Left | Down |  Up  | Right|   :  |  "   |
    * |------+------+------+------+------+------+----------------------------+------+------+------+------+------+------+------|
    * | Shift|   6  |   7  |   8  |   9  |   [  |       |                    |      |   ]  |   M  |   <  |   >  |   ?  | Shift|
    * |-------------+------+------+------+------+  Del  +------+------+------+ Bksp +------+------+------+------+-------------|
@@ -76,10 +78,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
   [_LOWER] = LAYOUT(
     KC_F11,  KC_F1,   KC_F2,   KC_F3,   KC_F4,  KC_F5,   _______,                        _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F12,
-    KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR, KC_MINS, _______,                        _______, KC_EQL,  KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE,
-    KC_TAB,  KC_1,    KC_2,    KC_3,    KC_TAB, KC_LBRC, _______,                        _______, KC_RBRC, KC_ENT,  KC_K,    KC_L,    KC_COLN, KC_DQT ,
+    KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR, KC_UNDS, _______,                        _______, KC_PLUS, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE,
+    KC_TAB,  KC_1,    KC_2,    KC_3,    KC_TAB, KC_LBRC, _______,                        _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,    KC_COLN, KC_DQT ,
     KC_LSFT, KC_6,    KC_7,    KC_8,    KC_9,   KC_LBRC, _______,                        _______, KC_RBRC, KC_M,    KC_LT,   KC_GT,   KC_QUES, KC_RSFT,
-    KC_LCTL, KC_LGUI, KC_LALT, CODE,            LOWER,   KC_DEL,  KC_LSFT,      KC_RSFT, KC_BSPC, RAISE,             KC_HOME, KC_PGDN, KC_PGUP, KC_END
+    KC_LCTL, KC_LGUI, KC_LALT, CODE,            LOWER,   KC_DEL,  KC_LSFT,      KC_RSFT, KC_BSPC, RAISE,            HACK,    KC_PGDN, KC_PGUP, KC_END
   ),
 
   /* Adjust
@@ -90,7 +92,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
    * |      |      | BL ON| BRTG | INC  |  DEC |      |                    |      |      | VOL- | VOL+ |      |      |      |
    * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
-   * |      |      |      |      |      |      |      |                    |      |      |      | Mute |      |      |      |
+   * |      |      |      |      |      |      |      |                    |      |      |      | Mute | Prev | Next |      |
    * |-------------+------+------+------+------+------+------+------+------+------+------+------+------+------+-------------|
    * |      |      |      |      ||||||||      |      |      ||||||||      |      |      ||||||||      |      |      |      |
    * ,----------------------------------------------------------------------------------------------------------------------.
@@ -99,7 +101,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______, _______, _______, _______, _______,_______,                       _______, _______, _______, _______, _______, _______, _______,
     _______, QK_BOOT, RGB_TOG, RGB_MOD, RGB_HUD, RGB_HUI,_______,                       _______, RGB_SAD, RGB_SAI, RGB_VAD, RGB_VAI, KC_MPLY, _______,
     _______, _______, BL_TOGG, BL_BRTG, BL_UP  , BL_DOWN,_______,                       _______, _______, KC_VOLD, KC_VOLU, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______,_______,                       _______, _______, KC_MUTE, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______,_______,                       _______, _______, KC_MUTE, KC_MPRV, KC_MNXT, _______, _______,
     _______, _______, _______, _______,          _______,_______,_______,       _______,_______, _______,          _______, _______, _______, _______
   ),
 
@@ -109,6 +111,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______, _______, RCS(KC_E), _______, RCS(KC_T),_______,                       _______, _______, _______, _______, _______,RCS(KC_P), _______,
     _______, _______, _______, RCS(KC_D), RCS(KC_F), RCS(KC_G),_______,                       _______, _______, _______, _______, _______, _______, _______,
     _______, _______, RCS(KC_X), RCS(KC_C), _______, _______,_______,                       _______, RCS(KC_M),_______, _______, _______, _______, _______,
+    _______, _______, _______, _______,          _______,_______,_______,       _______,_______, _______,          _______, _______, _______, _______
+  ),
+
+  // Adjust is kind of an annoying chord so we're using Hack layer for some of them.
+  [_HACK] = LAYOUT(
+    _______, _______, _______, _______, _______, _______,_______,                       _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______,_______,                       _______, _______, _______, _______, _______, KC_MPLY, _______,
+    _______, _______, _______, _______, _______, _______,_______,                       _______, _______, KC_VOLD, KC_VOLU, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______,_______,                       _______, _______, KC_MUTE, KC_MPRV, KC_MNXT, _______, _______,
     _______, _______, _______, _______,          _______,_______,_______,       _______,_______, _______,          _______, _______, _______, _______
   )
 };
@@ -171,6 +182,14 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         layer_on(_CODE);
       } else {
         layer_off(_CODE);
+      }
+      return false;
+      break;
+    case HACK:
+      if (record->event.pressed) {
+        layer_on(_HACK);
+      } else {
+        layer_off(_HACK);
       }
       return false;
       break;
